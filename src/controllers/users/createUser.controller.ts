e2a7fff76e8685f6dcdd3621b456d0c213224aed
Diego Rojas <devup2332@gmail.com>
2022-03-26 18:49:53 -0500

    develop: Edited docker compose file toa void conficts between api and client container

--- src/controllers/users/createUser.controller.ts
+++ src/controllers/users/createUser.controller.ts
@@ -6,84 +6,99 @@ import { User } from "../../models/users";
 
 const roles = ["user", "superadmin"];
 export const CreaterUserController = async (req: Request, res: Response) => {
-    try {
-        const { username, password, email, fullname, role, about_me, phone } = req.body;
-        if (!username || !password || !fullname || !role || !about_me || !phone || !email) {
-            return res.status(400).json({
-                message: "Please send complete data",
-                status: 400,
-            });
-        }
-        const user = (await pool.query("SELECT * FROM users WHERE username=?", [username])) as any;
-        if (user[0][0]) {
-            return res.status(200).json({
-                status: 200,
-                message: "Username already exist",
-            });
-        }
-        const [rolUser] = roles.filter((rol) => {
-            return rol === role;
-        });
-        if (!rolUser)
-            return res.status(400).json({
-                message: "Invalid Role",
-                status: 400,
-            });
-        if (role === "superadmin") {
-            const response = (await pool.query("SELECT * FROM users WHERE role='superadmin'")) as any;
-            const superadmin = response[0][0] as User;
-            if (superadmin)
-                return res.status(200).json({
-                    status: 200,
-                    message: "There is a superadmin user already",
-                });
-            const hash = await hashPassword(password);
-            const newUser: User = {
-                username,
-                password: hash,
-                fullname,
-                email,
-                role,
-                about_me,
-                phone,
-            };
-            const created = (await pool.query("INSERT INTO users set ?", [newUser])) as any;
-
-            const links = {
-                github: "",
-                linkedin: "",
-                youtube: "",
-                website: "",
-                facebook: "",
-                instagram: "",
-                user_id: created[0].insertId,
-            };
-            await pool.query("INSERT INTO social_links set ?", [links]);
-            return res.status(200).json({
-                status: 200,
-                message: "New Superadmin created Successfully",
-            });
-        }
-        const hash = await hashPassword(password);
-        const new_user = {
-            username,
-            password: hash,
-            fullname,
-            email,
-            role,
-            about_me,
-            phone,
-        };
-        await pool.query("INSERT INTO users set ?", [new_user]);
-        return res.status(200).json({
-            status: 200,
-            message: "New User created Successfully",
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            message: "Server internal error",
-            status: 500,
-        });
+  try {
+    const { username, password, email, fullname, role, about_me, phone } =
+      req.body;
+    if (
+      !username ||
+      !password ||
+      !fullname ||
+      !role ||
+      !about_me ||
+      !phone ||
+      !email
+    ) {
+      return res.status(400).json({
+        message: "Please send complete data",
+        status: 400,
+      });
     }
+    const user = (await pool.query("SELECT * FROM users WHERE username=?", [
+      username,
+    ])) as any;
+    if (user[0][0]) {
+      return res.status(200).json({
+        status: 200,
+        message: "Username already exist",
+      });
+    }
+    const [rolUser] = roles.filter((rol) => {
+      return rol === role;
+    });
+    if (!rolUser)
+      return res.status(400).json({
+        message: "Invalid Role",
+        status: 400,
+      });
+    if (role === "superadmin") {
+      const response = (await pool.query(
+        "SELECT * FROM users WHERE role='superadmin'"
+      )) as any;
+      const superadmin = response[0][0] as User;
+      if (superadmin)
+        return res.status(200).json({
+          status: 200,
+          message: "There is a superadmin user already",
+        });
+      const hash = await hashPassword(password);
+      const newUser: User = {
+        username,
+        password: hash,
+        fullname,
+        email,
+        role,
+        about_me,
+        phone,
+      };
+      const created = (await pool.query("INSERT INTO users set ?", [
+        newUser,
+      ])) as any;
+
+      const links = {
+        github: "",
+        linkedin: "",
+        youtube: "",
+        website: "",
+        facebook: "",
+        instagram: "",
+        user_id: created[0].insertId,
+      };
+      await pool.query("INSERT INTO social_links set ?", [links]);
+      return res.status(200).json({
+        status: 200,
+        message: "New Superadmin created Successfully",
+      });
+    }
+    const hash = await hashPassword(password);
+    const new_user = {
+      username,
+      password: hash,
+      fullname,
+      email,
+      role,
+      about_me,
+      phone,
+    };
+    await pool.query("INSERT INTO users set ?", [new_user]);
+    return res.status(200).json({
+      status: 200,
+      message: "New User created Successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Server internal error",
+      status: 500,
+    });
+  }
 };
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -4,14 +4,30 @@ import { CreateAdminController } from "../controllers/users/createAdmin.controll
 import { CreaterUserController } from "../controllers/users/createUser.controller";
 import { GetSuperadminController } from "../controllers/users/getSuperAdminProfile";
 import { GetUsersController } from "../controllers/users/getUsers.controller";
-import {UpdateUserController} from "../controllers/users/updateUser.controller";
+import { UpdateUserController } from "../controllers/users/updateUser.controller";
 
 const router = Router();
 
-router.get("/", passport.authenticate("jwt", { session: false }), GetUsersController);
-router.get("/superadmin", passport.authenticate("jwt", { session: false }), GetSuperadminController);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  GetUsersController
+);
+router.get(
+  "/superadmin",
+  passport.authenticate("jwt", { session: false }),
+  GetSuperadminController
+);
 router.post("/", CreaterUserController);
-router.post("/admin", passport.authenticate("jwt", { session: false }), CreateAdminController);
-router.put("/", passport.authenticate("jwt", { session: false }), UpdateUserController);
+router.post(
+  "/admin",
+  passport.authenticate("jwt", { session: false }),
+  CreateAdminController
+);
+router.put(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  UpdateUserController
+);
 
 export default router;
