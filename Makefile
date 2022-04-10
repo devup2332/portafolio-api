5974952104f78527957258d93d91bd9177585dff
Diego Rojas <devup2332@gmail.com>
2022-04-10 13:19:26 -0500

    UPP-03: Edited docker-compose file to run correctly in windows os

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ build-prod:
 	docker-compose -f docker-compose.prod.yml build
 run-prod:
 	docker-compose -f docker-compose.prod.yml up 
+
+run-db:
+	docker-compose -f docker-compose.dev-windows.yml up -d --remove-orphans && yarn dev
--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,19 @@
 import mysql from "mysql2";
 import { environments } from "./environments";
-console.log(environments.DB);
-
-const pool = mysql.createPool({
-  host: environments.DB.DB_HOST,
-  password: environments.DB.DB_PASSWORD,
-  user: environments.DB.DB_USER,
-  database: environments.DB.DB_DATABASE,
-});
-
-const promisePool = pool.promise();
-
-const connection = async () => {
-  pool.getConnection((err) => {
-    if (err) return console.log("Database fail", err);
-    console.log("Database is ready");
+
+const pool = mysql.createPool({
+  host: environments.DB.DB_HOST,
+  password: environments.DB.DB_PASSWORD,
+  user: environments.DB.DB_USER,
+  database: environments.DB.DB_DATABASE,
+});
+
+const promisePool = pool.promise();
+
+const connection = async () => {
+  pool.getConnection((err) => {
+    if (err) return console.log("Database fail", err);
+    console.log("Database is ready");
   });
 };
 
